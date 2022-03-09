e56c2323cf6cb2417e20e31e2e45de4220a464ce
Zt2tzzt <584633819@qq.com>
2022-03-10 00:05:44 +0800

    zzthahah

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
+/*
+ * @Description: file content
+ * @Author: ZeT1an
+ * @Date: 2022-03-09 22:25:16
+ * @LastEditors: ZeT1an
+ * @LastEditTime: 2022-03-09 23:11:49
+ * @LastEditContent: 'plugin:prettier/recommended'
+ */
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/typescript/recommended',
+		'plugin:prettier/recommended',
+		'plugin:prettier/recommended'
+	],
+	parserOptions: {
+		ecmaVersion: 2020
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+	}
+}
