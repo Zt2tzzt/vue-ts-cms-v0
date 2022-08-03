f1f22f2dd9bbefbeb323b2ecf2211a28e6d6947e
ZeT1an <zetian.zhu@cimc.com>
2022-08-03 20:49:24 +0800

    test: commitlint

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script lang="ts">
-import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn' //
 import { defineComponent } from 'vue'
 // 使用TS需要引入defineComponent函数
 export default defineComponent({
